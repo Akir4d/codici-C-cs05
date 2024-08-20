0a87b76d78670394c8b8622d1fbde5e9a7b5dea1
Paolo Rampino <liadserv@gmail.com>
2024-08-20 07:15:27 -0400

    prova

---
+++ puntatori.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (){
+    int i = 5;
+    int* p = &i;
+    printf("\nindirizzo di memoria 0x%x = %d", p, i);
+    *p = 7;
+    printf("\nindirizzo di memoria 0x%x = %d", p, i);
+    return 0;
+}
---
+++ vettori.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void scrivivettore(int *ptr)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        printf("\nInserisci un numero: ");
+        scanf("%d", ptr);
+        ptr++;
+    }
+}
+
+void leggivettore(int *ptr)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        printf("\nElemento %d vettore (0x%x): %d", i, ptr, *ptr);
+        ptr++;
+    }
+}
+
+int main()
+{
+    int n[10] = {0};
+    int *n_ptr;
+    n_ptr = &n[0];
+    scrivivettore(n_ptr);
+    leggivettore(n_ptr);
+    return 0;
+}
